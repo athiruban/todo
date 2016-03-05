660c61a8974130d28ff5f42a2b572eaba377fdb2
Athi Ruban <athi@freshdesk.com>
2016-03-06 02:07:06 +0530

    New and edit activity forms updated

--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,9 +1,45 @@
 require 'test_helper'
 
 class ActivitiesControllerTest < ActionController::TestCase
-  test "should get index" do 
+  def setup
+    @default = activities(:default)
+  end
+
+  test "should show all activities" do 
     get :index
-    assert_respones :success
+    assert_response :success
     assert_not_nil assigns(:activities)
   end
+
+  test "should create a new activity" do 
+    get :new
+    assert_response :success
+
+    activity_params = {
+      :content  => "Sample content",
+      :status   => Activity::Constants::ACTIVITY_STATUS[:open],
+      :priority => 3
+    }
+
+    assert_difference 'Activity.count' do 
+      post :create, :activity => activity_params
+    end
+  end
+
+  test "should edit the default activity" do 
+    get :edit, id: @default
+    assert_response :success
+
+    activity_params = {
+      :content  => @default.content,
+      :status   => Activity::Constants::ACTIVITY_STATUS[:closed],
+      :priority => 5      
+    }
+
+    post :update, id: @default, :activity => activity_params
+    assert_response :success
+
+    get :edit, id: @default
+    assert_response :success    
+  end  
 end
